adf33d41aad8b8f9d83de67a21c5c2ebf06c1b67
nabiamad29 <nabi2001@outlook.fr>
2023-12-11 16:55:58 +0100

    Initial commit

---
+++ fonction.h
@@ -0,0 +1,17 @@
+//
+// Created by nabiamad29 on 11/12/23.
+//
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <string.h>
+
+#ifndef TPRESV2_FONCTION_H
+#define TPRESV2_FONCTION_H
+
+#define msg "ceci n'est pas un fichier\n\r"
+
+#endif //TPRESV2_FONCTION_H
+
---
+++ main.c
@@ -0,0 +1,56 @@
+#include <unistd.h>
+#include "fonction.h"
+
+int main(int argc , char **argv) {
+    char buffer[128]={0};
+    char receive[128]={0};
+    char server[128]={0};
+    char rrq[128]={0};
+    char ACK[128]={0};
+    struct addrinfo hints;
+    struct addrinfo *res;
+    struct addrinfo *otherhints= calloc(1,sizeof(struct addrinfo));
+    struct sockaddr *socketrecv;
+    int soc,socketrecvSize;
+    char* Namefile=argv[1];
+    char* serv=argv[2];
+    char* port=argv[3];
+
+    if(argc!=4)
+    {
+        fprintf(stderr,"Usage:%s port\n",argv[0]);
+    }
+
+    memset(&hints,0,sizeof(struct addrinfo));
+    hints.ai_flags=0;
+    hints.ai_next=NULL;
+    hints.ai_socktype=SOCK_DGRAM;
+    hints.ai_family=AF_UNSPEC;
+    hints.ai_protocol=IPPROTO_UDP;
+
+    printf("le nom du fichier est %s\n\r",Namefile);
+    printf("le nom du serveur est %s \n\r",serv);
+    printf("le nom du port est %s\n\r",port);
+
+    getaddrinfo(serv, port, &hints, &res);
+    getnameinfo(res->ai_addr,res->ai_addrlen,buffer,128,server,128,NI_NUMERICHOST | NI_NUMERICSERV);
+
+    printf("serveur :%s:%s\n",buffer,server);
+    
+    soc=socket(res->ai_family,res->ai_socktype,res->ai_protocol);
+    if(soc==-1)
+    {
+        perror("erreur");
+        exit(EXIT_FAILURE);
+    }
+    printf("socket %d\n\r",soc);
+    sprintf(rrq, " \1%s netascii",Namefile);
+    rrq[0]=0;
+    rrq[strlen(Namefile)+2]=0;
+
+    sendto(soc,buffer,128,res->ai_flags,res->ai_addr,res->ai_addrlen);
+    recvfrom(soc,receive,128,res->ai_flags,&socketrecv,&socketrecvSize);
+
+
+    return 0;
+}
